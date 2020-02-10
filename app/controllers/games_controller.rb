f50d973e370bc36f39cf1fb247239236d8c46b2a
Leo Guezi <leo.guezi@protonmail.com>
2020-02-10 16:25:16 +0100

    presque

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,33 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    alphabet = ('a'...'z').to_a
+    10.times do
+      @letters << alphabet.sample
+    end
+  end
+
+  def score
+    @param = params[:guess]
+    @answer = ''
+    url = "https://wagon-dictionary.herokuapp.com/#{@param}"
+    dictionnary = open(url).read
+    user = JSON.parse(dictionnary)
+    @final = user["found"]
+    if @final == false
+      @answer = "Sorry but #{@param} does not seem to be a valid English word"
+    else
+      attempt = @param.split(//)
+      attempt.each do |letter|
+        if params[:letters].split(//).include?(letter) == false
+          @answer = 'the given word is not in the grid'
+        else
+          @answer = 'Good guess'
+        end
+      end
+    end
+  end
+end
